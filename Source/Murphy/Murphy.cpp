680c0070f0cf47d9ac3b4e272b4050126fa2bf1b
tang-ka <gur0907@hanyang.ac.kr>
2026-05-26 17:57:50 +0900

    Initialize Project

---
+++ Source/Murphy/Murphy.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Murphy.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Murphy, "Murphy" );

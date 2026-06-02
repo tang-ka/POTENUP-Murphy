e1302d53b7842947d01d19b48e8e22d1a18b6d16
tang-ka <gur0907@hanyang.ac.kr>
2026-06-02 21:38:29 +0900

    fix: 빌드 문제 해결

--- Source/Murphy/Murphy.Build.cs
+++ Source/Murphy/Murphy.Build.cs
@@ -26,8 +26,6 @@ public class Murphy : ModuleRules
 			"UMG",
 		});
 
-		PrivateDependencyModuleNames.AddRange(new string[] { "AudioCapture", "AudioCapture" });
-
 		// Uncomment if you are using Slate UI
 		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 		
